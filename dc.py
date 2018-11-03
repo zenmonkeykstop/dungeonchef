1a76e2b41c0ade56e14aab1f8ecf5016edcf05d9
Kevin O Gorman <kevin.ogorman@gmail.com>
2018-11-03 17:40:26 -0400

    continued cheffing

--- dc.py
+++ dc.py
@@ -5,6 +5,43 @@ SCREEN_HEIGHT=50
 LIMIT_FPS=20
 fullscreen = False
 
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+color_dark_wall = tcod.Color(172, 121, 101)
+color_dark_ground = tcod.Color(193, 154, 165)
+
+class Tile:
+    def __init__(self, blocked, block_sight = None):
+        self.blocked = blocked
+
+        block_sight = blocked if block_sight is None else None
+        self.block_sight = block_sight
+
+
+class Object:
+    # generic game objects
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        if not map[self.x +dx][self.y +dy].blocked:
+            self.x += dx
+            self.y += dy
+        else:
+            self.x -= 3*dx
+            self.y -= 3*dy
+
+    def draw(self):
+        tcod.console_set_default_foreground(con, self.color)
+        tcod.console_put_char(con, self.x, self.y, self.char, tcod.BKGND_NONE)
+
+    def clear(self):
+        tcod.console_put_char(con, self.x, self.y, '.', tcod.BKGND_NONE)
+
 def initialize_game():
     font_path = 'arial10x10.png'
     font_flags = tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD
@@ -16,12 +53,8 @@ def initialize_game():
 
     tcod.sys_set_fps(LIMIT_FPS)
 
-    global player_x, player_y
-    player_x = int(SCREEN_WIDTH/2)
-    player_y = int(SCREEN_HEIGHT/2)
-
 def handle_keys():
-    global player_x, player_y
+    global player
 
     # REAL TIME PLAY
     key = tcod.console_check_for_keypress()
@@ -33,26 +66,60 @@ def handle_keys():
 
 
     if tcod.console_is_key_pressed(tcod.KEY_UP):
-        player_y -= 1
+        player.move(0, -1)
     elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
-        player_y += 1
+        player.move(0, 1)
     elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
-        player_x -= 1
+        player.move(-1, 0)
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
-        player_x += 1
-    
+        player.move(1,0)
+
+def make_map():
+    global map
+
+    map = [
+            [Tile(False) for y in range(MAP_HEIGHT)]
+            for x in range(MAP_WIDTH)
+            ]
+
+    map[30][22].blocked =True
+    map[30][22].block_sight = True
+    map[50][22].blocked = True
+    map[50][22].block_sight = True
+
+npc = Object(SCREEN_WIDTH //2 -5, SCREEN_HEIGHT //2, '@', tcod.yellow)
+player = Object(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', tcod.white)
+objects = [npc, player]
+
+def render_all():
+    for object in objects:
+        object.draw()
+
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                tcod.console_set_char_background(con, x, y, color_dark_wall, 
+                        tcod.BKGND_SET)
+            else:
+                tcod.console_set_char_background(con, x, y, color_dark_ground,
+                        tcod.BKGND_SET)
+
+    tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
 
 def main():
     global con
     initialize_game()
+    make_map()
 
     exit_game = False
     while not tcod.console_is_window_closed() and not exit_game:
         tcod.console_set_default_foreground(con, tcod.white)
-        tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
-        tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+        render_all()
         tcod.console_flush()
-        tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
+        for object in objects:
+            object.clear()
         exit_game = handle_keys()
 
 main()
